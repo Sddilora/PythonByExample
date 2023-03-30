5e9d2bf18da950d3d59272e55aaf5eb172698492
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-30 03:52:52 +0300

    Initial commit

---
+++ variable_types/variables.py
@@ -0,0 +1,16 @@
+a = 85
+print(a)
+print (type(a)) # We don't indicate the variables type so if we wanna check this will print the variables type
+
+float(a)
+print (type(a))
+
+b = "Hello World!"
+print(b)
+
+c = True
+print (type(c))
+
+d = 1.0
+print(type(d))
+
